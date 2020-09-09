d448288b5f923264941c4e42aa5b0e2ecffea0fd
Jcanotorr06 <jcano062001@gmail.com>
2020-09-09 10:53:15 -0500

    Added automatic year change and message for the day

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const messageEl = document.getElementById('message');
 
 const newYears = '1 Jan 2021';
-
 function countdown(){
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
+    var newYearsDate = new Date(newYears);
+    var currentDate = new Date();
+    var currentYear = 2020;
+    if(currentDate > newYearsDate){
+      currentYear = currentYear+1
+      newYearsDate.setFullYear(currentYear);
+    }
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const months = Math.floor(totalSeconds / 3600 / 24 / 30.417);
@@ -23,11 +27,27 @@ function countdown(){
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
+
+    if(newYearsDate.getFullYear() == currentDate.getFullYear()){
+      if(newYearsDate.getMonth() == currentDate.getMonth()){
+        if(newYearsDate.getDate() == currentDate.getDate()){
+          monthsEl.innerHTML = '0'
+          daysEl.innerHTML = '0'
+          hoursEl.innerHTML = '0'
+          minutesEl.innerHTML = '0'
+          secondsEl.innerHTML = '0'
+          messageEl.innerHTML = 'Happy New Years'
+        }
+      }
+    }
+
 }
+
 
 function formatTime(time){
   return time < 10 ? (`0${time}`) : time;
 }
+
 
 countdown();
 
